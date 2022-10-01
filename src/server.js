1c0a0e3aab15451e54ce7cf3b8ac39e55237eff5
Jedai Saboteur <paradox.inversion@gmail.com>
2022-10-01 12:11:15 -0700

    Formatted server

--- src/server.js
+++ src/server.js
@@ -13,32 +13,34 @@ const LocalStrategy = require("passport-local");
 const User = require("./db/schema/user");
 const bcrypt = require("bcryptjs")
 const Bree = require('bree');
-var cors = require('cors')
+const cors = require('cors');
+const logRequest = require('./middleware/logRequest');
 
 connectDatabase();
 app.use(cors())
 // Start jobs for alerting
-const bree = new Bree({jobs: 
-  [
-    { 
-      name: "checkExpirations",
-      interval: "every hour"
-    }
-  ]
+const bree = new Bree({
+  jobs:
+    [
+      {
+        name: "checkExpirations",
+        interval: "every 20 seconds"
+      }
+    ]
 });
 
-(async () => {
-  await bree.start();
-})();
+// (async () => {
+//   await bree.start();
+// })();
 
 // Prepare standard middleware
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
-    secret: config.application.sessionSecret,
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
+  secret: config.application.sessionSecret,
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
 }));
 
 app.use(passport.initialize());
@@ -66,34 +68,34 @@ passport.use(
   })
 );
 
-passport.serializeUser(function(user, cb){
-    process.nextTick(function(){
-        cb(null, {id: user.id, user: user.username});
-    })
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, user: user.username });
+  })
 });
 
-passport.deserializeUser(function(user, cb){
-    process.nextTick(function(){
-        cb(null, {id: user.id, user: user.username});
-    })
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, user: user.username });
+  })
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', logRequest, (req, res) => {
+  res.send('Hello World!')
 })
 
 app.use("/api/v1", api);
 
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.json({error: err});
-  });
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
 
 app.listen(port, () => {
-    console.log(`Waste Not instance started on port ${port}`)
+  console.log(`Waste Not instance started on port ${port}`)
 })
