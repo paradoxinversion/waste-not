02b523e8a26a6fe83fbbd97a7abf776831d753da
Jedai Saboteur <paradox.inversion@gmail.com>
2022-10-01 11:45:07 -0700

    Updated checkExpirations

--- jobs/checkExpirations.js
+++ jobs/checkExpirations.js
@@ -1,1 +1,17 @@
-console.log("Checking Expiration Dates...")
+const connect = require("../src/db/connect-db")
+const InventoryItem = require("../src/db/schema/inventoryItem")
+
+
+const checkExpiry = async () =>{
+    try{
+        connect();
+        const inventory = await InventoryItem.find({expirationDate: {$lt: new Date(Date.now())}, expiryAlertDispatched: false});
+        inventory.forEach(item => {
+            console.log("Dispatching for " + item.name);
+        })
+    }catch(e){
+        console.error(e);
+    }
+}
+
+// checkExpiry()
