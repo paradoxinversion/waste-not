1303be72536c29c2e999c666cfd09b2b170a2559
Jedai Saboteur <paradox.inversion@gmail.com>
2022-11-09 10:43:30 -0800

    Updated connection logic

--- src/db/connect-db.js
+++ src/db/connect-db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose")
 const configurator = require("../config");
 const config = configurator();
-
 const connString = config.database.mongodb_connection_string;
 
 const connect = () => {
-  console.log("Connecting to Waste Not MongoDB Instance");
-  mongoose.connect(`${connString}/waste-not`).then(
+  console.log(`Connecting to Waste Not MongoDB Instance ${connString}`);
+  mongoose.connect(`${connString}`).then(
     () => {
       console.log("Connected to MongoDB Instance.")
     },
